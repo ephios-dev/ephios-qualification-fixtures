c6cac5bfa396086a86d27330de114a7f620ad895
Felix Rindt <felix@rindt.me>
2025-07-20 12:19:35 +0200

    add mermaid, LRC/SRC/UBI, DRSA-S includes EH

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ephios-qualification-fixtures
+
 Contains fixtures of commonly used qualifications for ephios.
+
+## Building
+
+For every language, we have a json file per qualification category.
+Use `python collect.py --all` to unify them in a single `_all.json` file that can be used
+in the qualification importer in ephios.
+
+## Visualizing
+
+Use the `--visualize` flag with the collect script to generate 
+mermaid code visualizing the inclusions in every `_all.json`.
+
+```bash
+python collect.py --all --visualize
+```
+
+The code can be rendered to an image using mermaid-cli, e.g.:
+```bash
+mmdc -i de/_all.json.mermaid -o de/_all.svg
+```
+
+<img src="https://raw.githubusercontent.com/ephios-dev/ephios-qualification-fixtures/refs/heads/main/de/_all.svg" alt="graph visualization of german qualification fixtures">
--- collect.py
+++ collect.py
@@ -8,8 +8,8 @@ import argparse
 import json
 import re
 import sys
-import time
 from pathlib import Path
+
 
 TARGET_FILENAME = "_all.json"
 
@@ -36,6 +36,27 @@ def collect(path: Path):
         json.dump(all_objects, target_file, ensure_ascii=False)
 
 
+def visualize(path):
+    with open(path / TARGET_FILENAME, "r") as target_file:
+        all_objects = json.load(target_file)
+
+    all_objects = sorted(all_objects, key=lambda o: o["category"]["title"])
+    script = "flowchart LR\n"
+    edges = set()
+    for obj in all_objects:
+        uuid = obj['uuid']
+        script += f'\t{uuid}(["{obj['abbreviation']}"])\n'
+        for includes in obj['includes']:
+            edges.add((uuid, includes))
+        for included_by in obj['included_by']:
+            edges.add((included_by, uuid))
+    for a,b in edges:
+        script += f'\t{a} --> {b}\n'
+
+    with open(path / f"{TARGET_FILENAME}.mermaid" , "w") as mermaid_file:
+        mermaid_file.write(script)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=f"Concatenate lists in every json file contained in the input paths together in one json file called `{TARGET_FILENAME}` "
@@ -44,6 +65,7 @@ def main():
     input_group = parser.add_mutually_exclusive_group(required=True)
     input_group.add_argument("-p", "--paths", nargs="+", type=Path, dest="paths", metavar="PATH")
     input_group.add_argument("-a", "--all", action="store_true", dest="collect_all", help="collect from all two-letter subdirectories of the working directory")
+    parser.add_argument("-v", "--visualize", action="store_true", dest="visualize", help="generate mermaid.js code for the inclusion graph")
 
     args = parser.parse_args()
 
@@ -58,6 +80,8 @@ def main():
     for path in paths:
         print(f"Collecting '{path!s}': ", end="", flush=True)
         collect(path)
+        if args.visualize:
+            visualize(path)
         print("DONE")
 
 
